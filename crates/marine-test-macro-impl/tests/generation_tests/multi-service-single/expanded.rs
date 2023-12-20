05321fe7b0e09fd95f2b190e08a10fb278f83779
Valery Antopol <valery.antopol@gmail.com>
2023-12-20 16:50:14 +0400

    fix: allow unused imports in marine-test-env (#111)
    allow unused imports in marine-test-env

--- crates/marine-test-macro-impl/tests/generation_tests/multi-service-single/expanded.rs
+++ crates/marine-test-macro-impl/tests/generation_tests/multi-service-single/expanded.rs
@@ -2,6 +2,7 @@
 fn empty_test() {
     #[allow(unused)]
     #[allow(non_snake_case)]
+    #[allow(unused_imports)]
     pub mod marine_test_env {
         pub mod empty_func {
             pub mod modules {
